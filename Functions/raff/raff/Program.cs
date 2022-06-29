a27e648b98664d88a2a30bde8b795b602179a8b4
sahib967 <106153761+sahib967@users.noreply.github.com>
2022-06-29 12:54:07 +0500

    Functions

---
+++ Functions/raff/raff/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace function
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int value = increase(50);
+            Console.WriteLine("New value:" + value + "\n");
+            int result = increase(10) * 5;
+            Console.WriteLine("result:" + result + "\n");
+            int marks = 80;
+            Console.WriteLine(increase(marks));
+            Console.ReadKey();
+        }
+        static int increase(int number) 
+        {
+            Console.WriteLine("i am print function");
+            Console.WriteLine("i recieved:"+number);
+            return number + 10;
+
+        }
+    }
+}
