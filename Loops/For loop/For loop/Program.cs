6981a1c7bc4e42ad623422118d893e8aa6477516
sahib967 <106153761+sahib967@users.noreply.github.com>
2022-06-29 12:56:02 +0500

    Loops

---
+++ Loops/For loop/For loop/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace For_loop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int i = 0;
+            for (i = 0; i < 5; i++) ;
+            {
+                
+                Console.WriteLine(i);
+            }
+            Console.ReadLine();
+        }
+    }
+}
